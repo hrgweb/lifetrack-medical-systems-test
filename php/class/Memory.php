847bf79741dd869940f9d915da7c43600c3fa8fa
hrgweb <gamayonhergenroque.web@gmail.com>
2021-01-12 23:18:13 +0800

    update props on Memory class

--- php/class/Memory.php
+++ php/class/Memory.php
@@ -2,10 +2,10 @@
 
 class Memory
 {
-  const COST_PER_STUDY = 10;
-  const COST_PER_1000_STUDIES = 500;
-  const ONE_GB_COST_PER_HOUR = 0.00553;
-  const ONE_GB_COST_PER_MONTH = 0.10;
+  protected const COST_PER_STUDY = 10;  // 10mb
+  protected const COST_PER_1000_STUDIES = 500; // 500mb
+  protected const ONE_GB_COST_PER_HOUR = 0.00553;
+  protected const ONE_GB_COST_PER_MONTH = 0.10;
 
   public function costPerStudy()
   {
