cc891ae3b27b79ad3d9c68fd217468bb8057272b
hrgweb <gamayonhergenroque.web@gmail.com>
2021-01-13 21:39:05 +0800

    update execution file

--- php/submit.php
+++ php/submit.php
@@ -1,11 +1,5 @@
 <?php
 
-// IMPORT CLASS
-include_once './class/Study.php';
-include_once './class/Table.php';
+include_once 'class/Table.php';
 
-// CLASS INSTANTIATION
-$study = new Study();
-$table = new Table();
-
-echo $table->display($_GET);
+echo (new Table())->display($_GET);
