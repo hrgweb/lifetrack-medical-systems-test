49d798bce9a58e383db3a6a49a603fcd8c3513e2
hrgweb <gamayonhergenroque.web@gmail.com>
2021-01-13 17:14:30 +0800

    add validation when inputs are empty and segregate data function

--- js/script.js
+++ js/script.js
@@ -1,11 +1,40 @@
 function data() {
-  var inputs = {
+  return {
     studyPerDay: document.getElementById("study_per_day").value,
     studyGrowthPerMonth: document.getElementById("study_growth_per_month")
       .value,
     monthsToForecast: document.getElementById("months_to_forecast").value,
   };
+}
 
+function validation() {
+  var input = data();
+
+  // CHECK IF STUDY PER DAY IS EMPTY
+  if (!input.studyPerDay) {
+    alert("Study per day is empty");
+
+    return false;
+  }
+
+  // CHECK IF STUDY GROWTH PER MONTH IS EMPTY
+  if (!input.studyGrowthPerMonth) {
+    alert("Study growth per month (%) is empty");
+
+    return false;
+  }
+
+  // CHECK IF STUDY GROWTH PER MONTH IS EMPTY
+  if (!input.monthsToForecast) {
+    alert("Months to forecast is empty");
+
+    return false;
+  }
+
+  return true;
+}
+
+function queryString() {
   var queryStr = "?";
 
   queryStr += "studyPerDay=" + inputs.studyPerDay + "&";
@@ -22,6 +51,9 @@ function src(file) {
 // SUBMIT THE FORM
 document.getElementById("submit").addEventListener("click", function (e) {
   e.preventDefault();
+
+  console.log(validation());
+  return;
 
   var xmlhttp = new XMLHttpRequest();
 
