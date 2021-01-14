5af4da2501c6e8cff9c68e033f1d06e9600d7740
hrgweb <gamayonhergenroque.web@gmail.com>
2021-01-14 19:51:37 +0800

    make Study.php class handle cost forecasted logic and clean up unnecesarry code

--- php/class/Study.php
+++ php/class/Study.php
@@ -1,7 +1,18 @@
 <?php
 
+include_once 'Memory.php';
+include_once 'Storage.php';
 class Study
 {
+  protected $memory;
+  protected $storage;
+
+  public function __construct()
+  {
+    $this->memory = new Memory();
+    $this->storage = new Storage();
+  }
+
   public static function computeGrowthPerMonth(array $inputs)
   {
     $study_per_day = (float) $inputs['studyPerDay'];
@@ -10,5 +21,10 @@ class Study
     $percentage_growth = ($study_per_day * $inputted_growth_per_month) / 100;
 
     return $study_per_day + $percentage_growth;
+  }
+
+  public function costForecasted(array $inputs)
+  {
+    return number_format((float) $this->memory->costPerMonth($inputs) + (float) $this->storage->costPerMonth($inputs), 2);
   }
 }
